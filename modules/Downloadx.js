72314b037c49ad0b427eab55639e1dcecd09a238
是甜食哇 <54750485+shitianshiwa@users.noreply.github.com>
2020-11-28 21:28:56 +0900

    修复代理未起作用错误

--- modules/Downloadx.js
+++ modules/Downloadx.js
@@ -10,12 +10,16 @@ const PROXY_CONF = config.default.proxy; //发现套了一个default。。！
 var proxyip = false;
 if (PROXY_CONF.host.length > 0 && PROXY_CONF.port !== 0) {
     proxyip = "http://" + PROXY_CONF.host + ":" + PROXY_CONF.port;
+    //logger2.info(proxyip);
 }
 module.exports = async function Downloadx(url, name, i) {
     let fileDataArr = await new Promise(async function (resolve, reject) {
-        logger2.info("下载文件 , "+url+" , "+name+" , "+i.toString());
+        logger2.info("下载文件 , " + url + " , " + name + " , " + i.toString());
+        //logger2.info(proxyip != false ? proxyip : false);
         resolve(download(url, {
-            proxy: proxyip ? proxyip : false
+            proxy: proxyip != false ? proxyip : false
+        }).catch(err => {
+            logger2.error(new Date().toString() + " , " + err);
         }));
     });
     if (fileDataArr != null) {
