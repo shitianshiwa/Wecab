bba66c687bf713ac7a5ac0ffa472d9c92ab922e2
是甜食哇 <54750485+shitianshiwa@users.noreply.github.com>
2020-11-29 19:18:47 +0900

    Update Downloadx.js

--- modules/Downloadx.js
+++ modules/Downloadx.js
@@ -9,18 +9,21 @@ const logger2 = require('./logger2'); //日志功能
 const PROXY_CONF = config.default.proxy; //发现套了一个default。。！
 var proxyip = false;
 if (PROXY_CONF.host.length > 0 && PROXY_CONF.port !== 0) {
-    proxyip = "http://" + PROXY_CONF.host + ":" + PROXY_CONF.port;
-    //logger2.info(proxyip);
+    proxyip = {
+        host: PROXY_CONF.host,
+        port: PROXY_CONF.port
+    }
+    //"http://" + PROXY_CONF.host + ":" + PROXY_CONF.port;
 }
 module.exports = async function Downloadx(url, name, i) {
     let fileDataArr = await new Promise(async function (resolve, reject) {
         logger2.info("下载文件 , " + url + " , " + name + " , " + i.toString());
-        //logger2.info(proxyip != false ? proxyip : false);
         resolve(download(url, {
-            proxy: proxyip != false ? proxyip : false
+            proxy: proxyip ? proxyip : false
         }).catch(err => {
             logger2.error(new Date().toString() + " , " + err);
         }));
+        //https://github.com/kevva/download/commit/a16ba04b30dafbe7d9246db93f1534320d8e0dd3 v8.0.0删掉代理功能了
     });
     if (fileDataArr != null) {
         const imgType = fileType(fileDataArr).ext;
